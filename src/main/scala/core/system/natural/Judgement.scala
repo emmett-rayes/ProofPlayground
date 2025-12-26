fc24fc742911790e2764beb0383c3dbbd24fdf84
Emmett Rayes <emmett.rayes@icloud.com>
2025-12-26 22:19:38 +0200

    Add Judgement representation for natural deduction

---
+++ src/main/scala/core/system/natural/Judgement.scala
@@ -0,0 +1,14 @@
+package proofPlayground
+package core.system.natural
+
+/** Representation of a judgement in natural deduction.
+ *
+ * A judgement consists of a set of assumptions and an assertion.
+ * A judgement in natural deduction has a single formula in the conclusion.
+ *
+ * @tparam S the type constructor for the collection of assumptions
+ * @tparam F the type of formulas
+ * @param assumptions the collection of formulas assumed to be true
+ * @param assertion   the formula that is asserted
+ */
+case class Judgement[S[_], F](assumptions: S[F], assertion: F)
