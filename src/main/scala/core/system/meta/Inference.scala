8fde11ef307a576c0648cb99ddc24415b40f392e
Emmett Rayes <emmett.rayes@icloud.com>
2025-12-26 22:17:25 +0200

    Add Inference representation with hypotheses and conclusion

---
+++ src/main/scala/core/system/meta/Inference.scala
@@ -0,0 +1,10 @@
+package proofPlayground
+package core.system.meta
+
+/** General representation of a syntactical inference line
+ *
+ * @tparam J The type of judgments in this inference
+ * @param hypotheses The set of hypotheses above the line
+ * @param conclusion The conclusion below the line
+ */
+case class Inference[J](hypotheses: Set[J], conclusion: J)
