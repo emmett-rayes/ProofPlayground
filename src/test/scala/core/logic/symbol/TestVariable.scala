689c9dd5eb4c4ebb60a721cc90e57b23c1414519
Emmett Rayes <emmett.rayes@icloud.com>
2025-12-27 16:42:44 +0200

    Remove unnecessary test cases after privatizing Variable constructor

--- src/test/scala/core/logic/symbol/TestVariable.scala
+++ src/test/scala/core/logic/symbol/TestVariable.scala
@@ -9,18 +9,6 @@ final class TestVariable extends AnyFunSuite:
   /** A dummy kind used for testing variables. */
   private sealed trait DummyKind
 
-  test("a variable is equal to its copy") {
-    val v1 = Variable[DummyKind]()
-    val v2 = v1.copy()
-    assert(v1 === v2)
-  }
-
-  test("a variable and its copy have the same ids") {
-    val v1 = Variable[DummyKind]()
-    val v2 = v1.copy()
-    assert(v1.id === v2.id)
-  }
-
   test("two different variables are not equal") {
     val v1 = Variable[DummyKind]()
     val v2 = Variable[DummyKind]()
