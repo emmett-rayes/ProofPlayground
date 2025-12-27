d10dd1f0daae94dd29217138f013c36ffe2f4037
Emmett Rayes <emmett.rayes@icloud.com>
2025-12-27 16:45:47 +0200

    Add test cases for Variable to validate kind-specific behaviour

--- src/test/scala/core/logic/symbol/TestVariable.scala
+++ src/test/scala/core/logic/symbol/TestVariable.scala
@@ -6,17 +6,36 @@ import org.scalatest.funsuite.AnyFunSuite
 /** Tests for the [[Variable]] class and its companion object. */
 final class TestVariable extends AnyFunSuite:
 
-  /** A dummy kind used for testing variables. */
-  private sealed trait DummyKind
-
   test("two different variables are not equal") {
+    sealed trait DummyKind
+
     val v1 = Variable[DummyKind]()
     val v2 = Variable[DummyKind]()
     assert(v1 !== v2)
   }
 
   test("two different variables have different ids") {
+    sealed trait DummyKind
+
     val v1 = Variable[DummyKind]()
     val v2 = Variable[DummyKind]()
     assert(v1.id !== v2.id)
+  }
+
+  test("variables of different kinds have independent counters") {
+    sealed trait DummyKind1
+    sealed trait DummyKind2
+
+    val v1 = Variable[DummyKind1]()
+    val v2 = Variable[DummyKind2]()
+    assert(v1.id === v2.id)
+  }
+
+  test("variables of different kinds have invariant counters") {
+    sealed trait DummyKind1
+    sealed trait DummyKind2 extends DummyKind1
+
+    val v1 = Variable[DummyKind1]()
+    val v2 = Variable[DummyKind2]()
+    assert(v1.id === v2.id)
   }
