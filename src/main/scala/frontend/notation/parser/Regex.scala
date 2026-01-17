5bbd5185fd4da0f3d9850a26786ae6455a51d930
Emmett Rayes <emmett.rayes@icloud.com>
2026-01-17 23:55:37 +0200

    Add Regex parser implementation

---
+++ src/main/scala/frontend/notation/parser/Regex.scala
@@ -0,0 +1,17 @@
+package proofPlayground
+package frontend.notation.parser
+
+import scala.util.Try
+import scala.util.matching.Regex
+
+object Regex:
+  def parser(regex: Regex): Parser[Tokens, String] =
+    input =>
+      val trimmed = input.skipWhitespace
+      Try {
+        if trimmed.isEmpty then
+          throw ParseError(input, "Expected input at this position.")
+        regex.findPrefixMatchOf(trimmed.mkString) match
+          case None    => throw ParseError(input, s"Expected input that matches ${regex.regex}")
+          case Some(m) => (input.mkString.substring(m.end), input.mkString.substring(m.start, m.end))
+      }
