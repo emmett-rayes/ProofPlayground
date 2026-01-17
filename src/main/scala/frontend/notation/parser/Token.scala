f4e91ee0153460a0934b27f652e771196af4c916
Emmett Rayes <emmett.rayes@icloud.com>
2026-01-17 17:37:35 +0200

    Add Token related definitions

---
+++ src/main/scala/frontend/notation/parser/Token.scala
@@ -0,0 +1,22 @@
+package proofPlayground
+package frontend.notation.parser
+
+/** A token is a single character in the input string to be parsed. */
+type Token = Char
+
+/** A sequence of tokens representing the input string to be parsed. */
+type Tokens = IndexedSeq[Token]
+
+extension (self: String)
+  /** Converts the string into a sequence of tokens.
+    *
+    * @return the sequence of tokens representing the string.
+    */
+  def asTokens: Tokens = self
+
+extension (tokens: Tokens)
+  /** Skips leading whitespace tokens and returns the remaining tokens.
+    *
+    * @return the remaining tokens after skipping leading whitespace.
+    */
+  def skipWhitespace: Tokens = tokens.span(_.isWhitespace)._2
