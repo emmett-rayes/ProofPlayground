6f77048ead0dd3fd48b5b9fadbe6ca8546e9992b
Emmett Rayes <emmett.rayes@icloud.com>
2026-01-26 15:07:52 +0200

    Extract Coordinator to own file

--- src/main/scala/frontend/tui/App.scala
+++ src/main/scala/frontend/tui/App.scala
@@ -3,31 +3,6 @@ package frontend.tui
 
 import tui.withTerminal
 
-class Coordinator extends Navigation:
-  private var screens: List[Screen] = List.empty
-
-  {
-    navigateTo(Navigation.Screen.FormulaInput)
-  }
-
-  def shouldExit: Boolean   = screens.isEmpty
-  def currentScreen: Screen = screens.head
-
-  override def signalExit(): Unit =
-    screens = List.empty
-
-  override def navigateTo(destination: Navigation.Screen): Unit =
-    val screen = destination match
-      case Navigation.Screen.FormulaInput       => FormulaInput(this)
-      case Navigation.Screen.ProofTree(formula) => ProofTree(this)
-    screens = screen :: tail
-
-  override def showPopup(message: String, title: Option[String])(callback: () => Unit): Unit =
-    screens = Popup(message, title)(callback, { () => screens = tail }) :: screens
-
-  private def tail: List[Screen] =
-    if screens.isEmpty then List.empty else screens.tail
-
 @main
 def main(): Unit =
   val coordinator = Coordinator()
