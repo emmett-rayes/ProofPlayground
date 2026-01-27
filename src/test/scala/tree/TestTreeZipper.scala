fbedfb89074f5788f392a3ac9969233f1dd641ab
Emmett Rayes <emmett.rayes@icloud.com>
2026-01-27 23:47:48 +0200

    Add test cases for TreeZipper boundary conditions

--- src/test/scala/tree/TestTreeZipper.scala
+++ src/test/scala/tree/TestTreeZipper.scala
@@ -6,26 +6,49 @@ import org.scalatest.funsuite.AnyFunSuite
 class TestTreeZipper extends AnyFunSuite:
 
   test("down then up restores the original tree") {
-    val tree   = Tree.Node("A", List(Tree.Leaf("B"), Tree.Leaf("C")))
+    val tree   = Tree("A", List(Tree("B"), Tree("C")))
     val zipper = TreeZipper(tree)
     assert(zipper.down.flatMap(_.up) === Some(zipper))
   }
 
   test("down goes to left-most child") {
-    val tree   = Tree.Node("A", List(Tree.Leaf("B"), Tree.Leaf("C")))
+    val tree   = Tree("A", List(Tree("B"), Tree("C")))
     val zipper = TreeZipper(tree)
-    assert(zipper.down.map(_.get) === Some(Tree.Leaf("B")))
+    assert(zipper.down.map(_.get) === Some(Tree("B")))
   }
 
   test("right goes to the next sibling") {
-    val tree   = Tree.Node("A", List(Tree.Leaf("B"), Tree.Leaf("C")))
+    val tree   = Tree("A", List(Tree("B"), Tree("C")))
     val zipper = TreeZipper(tree)
-    assert(zipper.down.flatMap(_.right).map(_.get) === Some(Tree.Leaf("C")))
+    assert(zipper.down.flatMap(_.right).map(_.get) === Some(Tree("C")))
   }
 
   test("left goes to the previous sibling") {
-    val tree = Tree.Node("A", List(Tree.Leaf("B"), Tree.Leaf("C")))
+    val tree   = Tree("A", List(Tree("B"), Tree("C")))
     val zipper = TreeZipper(tree)
-    assert(zipper.down.flatMap(_.right).flatMap(_.left).map(_.get) === Some(Tree.Leaf("B")))
+    assert(zipper.down.flatMap(_.right).flatMap(_.left).map(_.get) === Some(Tree("B")))
   }
 
+  test("up from root returns None") {
+    val tree   = Tree("A", List(Tree("B"), Tree("C")))
+    val zipper = TreeZipper(tree)
+    assert(zipper.up === None)
+  }
+
+  test("down from leaf returns None") {
+    val tree   = Tree("A")
+    val zipper = TreeZipper(tree)
+    assert(zipper.down === None)
+  }
+
+  test("left from left-most child returns None") {
+    val tree   = Tree("A", List(Tree("B"), Tree("C")))
+    val zipper = TreeZipper(tree)
+    assert(zipper.down.flatMap(_.left) === None)
+  }
+
+  test("right from right-most child returns None") {
+    val tree   = Tree("A", List(Tree("B"), Tree("C")))
+    val zipper = TreeZipper(tree)
+    assert(zipper.down.flatMap(_.right).flatMap(_.right) === None)
+  }
