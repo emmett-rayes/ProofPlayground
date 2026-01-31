2c19c8bbdb4c6334fcc46e55d5f58a25cb706a53
Emmett Rayes <emmett.rayes@icloud.com>
2026-01-31 17:27:39 +0200

    Add Functor instance for Tree

--- src/main/scala/tree/Tree.scala
+++ src/main/scala/tree/Tree.scala
@@ -1,13 +1,14 @@
 package proofPlayground
 package tree
 
+import core.Functor
+
 /** A recursive tree data structure with an arbitrary number of children.
   *
   * A leaf node has no children.
   *
   * @note This definition contains the sum type implicitly.
   *       The corresponding functor of the least fixed point is X => A + List[X].
-  *
   * @tparam A the type of values stored in the tree.
   */
 case class Tree[+A](value: A, children: List[Tree[A]] = List.empty):
@@ -16,3 +17,10 @@ case class Tree[+A](value: A, children: List[Tree[A]] = List.empty):
     * @return true if this tree is a leaf node, false otherwise.
     */
   def isLeaf: Boolean = children.isEmpty
+
+object Tree:
+  /** [[Functor]] instance for [[Tree]]. */
+  given Functor[Tree]:
+    extension [A](fa: Tree[A])
+      def map[B](f: A => B): Tree[B] =
+        Tree(f(fa.value), fa.children.map(_.map(f)))
