4181c81d8beeea73897b710a4b294171631cdcca
Emmett Rayes <emmett.rayes@icloud.com>
2026-01-31 17:32:01 +0200

    Make Popup taller

--- src/main/scala/frontend/tui/Popup.scala
+++ src/main/scala/frontend/tui/Popup.scala
@@ -6,7 +6,8 @@ import tui.crossterm.{Event, KeyCode}
 import tui.widgets.{BlockWidget, ParagraphWidget}
 
 class Popup(message: String, title: Option[String])(confirm: () => Unit, dismiss: () => Unit) extends Screen:
-  private val size       = 30
+  private val ySize       = 40
+  private val xSize       = 30
   private var confirming = false
 
   override def headerText: Text =
@@ -33,7 +34,7 @@ class Popup(message: String, title: Option[String])(confirm: () => Unit, dismiss
     }
 
   override def render(renderer: Renderer, area: Rect): Unit =
-    val contentArea = Rectangle(size, area)
+    val contentArea = Rectangle(ySize, xSize, area)
 
     val contentLayout = Layout(
       direction = Direction.Vertical,
@@ -74,22 +75,22 @@ class Popup(message: String, title: Option[String])(confirm: () => Unit, dismiss
     renderer.render(cancelButton, buttonsLayout(0))
     renderer.render(confirmButton, buttonsLayout(2))
 
-  private def Rectangle(size: Int, area: Rect): Rect =
+  private def Rectangle(ySize: Int, xSize: Int, area: Rect): Rect =
     val yLayout = Layout(
       direction = Direction.Vertical,
       constraints = Array(
-        Constraint.Percentage((100 - size) / 2),
-        Constraint.Percentage(size),
-        Constraint.Percentage((100 - size) / 2)
+        Constraint.Percentage((100 - ySize) / 2),
+        Constraint.Percentage(ySize),
+        Constraint.Percentage((100 - ySize) / 2)
       )
     ).split(area)
 
     val xLayout = Layout(
       direction = Direction.Horizontal,
       constraints = Array(
-        Constraint.Percentage((100 - size) / 2),
-        Constraint.Percentage(size),
-        Constraint.Percentage((100 - size) / 2),
+        Constraint.Percentage((100 - xSize) / 2),
+        Constraint.Percentage(xSize),
+        Constraint.Percentage((100 - xSize) / 2),
       )
     ).split(yLayout(1))
 
