38ddbdf4d001f7a6fb51b29983faf04386dde271
Emmett Rayes <emmett.rayes@icloud.com>
2026-02-02 14:30:33 +0200

    Split the implementation of traverse to map and sequence

--- src/main/scala/core/Traverse.scala
+++ src/main/scala/core/Traverse.scala
@@ -1,10 +1,19 @@
 package proofPlayground
 package core
 
+/** Sequence a [[Seq]] of options with [[Option]]
+  * If any of the values in the sequence is None, the result is None.
+  * Otherwise, the result is a sequence of all values contained in [[Option]].
+  */
+def sequence[A](seq: Seq[Option[A]]): Option[Seq[A]] =
+  seq.foldLeft(Option(Seq.empty)) { (acc, a) =>
+    for b <- a; bs <- acc yield b +: bs
+  }
+
 extension [A](seq: Seq[A])
   /** Traverse a sequence of values using a function that produces an option.
-    * If any of the values in the sequence is None, the result is None.
-    * Otherwise, the result is a sequence of all produced values.
+    * If any of the values in the sequence maps to None, the result is None.
+    * Otherwise, the result is a sequence of all mapped values.
     *
     * @tparam A the type of the consumed values
     * @tparam B the type of the produced values
@@ -14,9 +23,4 @@ extension [A](seq: Seq[A])
     */
   // noinspection ScalaDocUnknownParameter
   def traverse[B](f: A => Option[B]): Option[Seq[B]] =
-    seq.foldLeft(Option(Seq.empty[B])) { (acc, a) =>
-      for
-        b  <- f(a)
-        bs <- acc
-      yield b +: bs
-    }
+    sequence[B](seq.map(f))
